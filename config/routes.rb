bafb458ac8799fce23e9d7d29817d14ba7850a66
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2017-02-01 02:45:55 -0600

    Home screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'home#show'
+
   get 'using-from-example/without-from', to: 'using_from_example#without_from'
   get 'using-from-example/with-from',    to: 'using_from_example#with_from'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
