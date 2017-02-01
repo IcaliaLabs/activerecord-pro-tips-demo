55dd8de98b1223f28732f3a78bb41e29a75dcc74
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2017-02-01 02:37:00 -0600

    First example regarding the ActiveRecord::Base.from trick

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'using-from-example/without-from', to: 'using_from_example#without_from'
+  get 'using-from-example/with-from',    to: 'using_from_example#with_from'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
