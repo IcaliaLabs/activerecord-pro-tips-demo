527abe58772a765801129a86bce3dc437ccfc884
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2017-02-01 15:32:00 -0600

    Reconfigured the app with most of rails modules except ActionMailer

--- config/application.rb
+++ config/application.rb
@@ -8,15 +8,15 @@ require 'active_record/railtie'
 require 'action_controller/railtie'
 # require 'action_mailer/railtie'
 require 'action_view/railtie'
-# require 'action_cable/engine'
+require 'action_cable/engine'
 require 'sprockets/railtie'
-# require 'rails/test_unit/railtie'
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ActiverecordFromExample
+module ArProTipsDemo
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_activerecord-from-example_session'
+Rails.application.config.session_store :cookie_store, key: '_ar-pro-tips-demo_session'
