0f15969689ee120add6dccd100ae5f3e57bec570
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2017-02-02 17:36:59 -0600

    Event filtering on ActiveRecordLogSubscriber

--- app/subscribers/active_record_log_subscriber.rb
+++ app/subscribers/active_record_log_subscriber.rb
@@ -2,11 +2,29 @@
 # Subscribes to the ActiveRecord notifications, and captures the issued SQL commands, redirecting
 # them to the user via ActionCable:
 class ActiveRecordLogSubscriber < ActiveSupport::LogSubscriber
-  def sql(event)
-    ActionCable.server.broadcast 'logger_output',
-                                 type: 'sql',
-                                 name: event.payload[:name],
-                                 duration: event.duration,
-                                 sql: event.payload[:sql]
+  IGNORED_QUERY_NAMES = [
+    'SCHEMA',
+    'ActiveRecord::SchemaMigration Load'
+  ].freeze
+
+  def self.extract_data_from_payload(payload)
+    data = payload.slice(:name, :sql)
+
+    # Replace any query parameter binding...
+    payload[:binds].each_with_index do |bind, index|
+      data[:sql] = data[:sql].gsub "$#{index + 1}", "'#{bind.value}'"
+    end
+
+    data
   end
+
+  def self.sql(event)
+    payload = event.payload
+    return if payload[:name].in? IGNORED_QUERY_NAMES
+    broadcast_data = { type: :sql, duration: event.duration }
+    broadcast_data.merge! extract_data_from_payload(payload)
+    ActionCable.server.broadcast 'logger_output', broadcast_data
+  end
+
+  delegate :sql, to: :class
 end
