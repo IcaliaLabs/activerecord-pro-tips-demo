e866792991d953bcab0dbb1cf332fb75eb788539
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2017-02-04 20:27:10 -0600

    Implemented 'available' scope on Product and Item models

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   validates :name, :brand, presence: true
   validates :name, uniqueness: { scope: :brand }
   belongs_to :category
+
+  has_many :items, inverse_of: :product
+
+  scope :available, -> { joins(:items).distinct.merge Item.available }
 end
